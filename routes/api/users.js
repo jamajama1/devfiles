9e99460862d61b74080485767036938ab14101bc
jamajama1 <jamajama@buffalo.edu>
2019-04-12 21:32:56 -0400

    Jwt / user authorization complete

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
+
 
 // Load user model
 const User = require('../../models/User');
@@ -90,6 +92,17 @@ router.post('/login', (req,res) => {
             }
         })
     });
+});
+
+// @route GET api/users/current
+// @desc return current user
+// @access Private
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+});
 });
 
 module.exports = router;
